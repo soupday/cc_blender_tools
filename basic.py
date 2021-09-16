cfb11adab4c91986a05c34f8a233e4f9a86c1f71
soupday <79094830+soupday@users.noreply.github.com>
2021-09-16 14:33:15 +0100

    1.0.2

--- basic.py
+++ basic.py
@@ -1,3 +1,19 @@
+# Copyright (C) 2021 Victor Soupday
+# This file is part of CC3_Blender_Tools <https://github.com/soupday/cc3_blender_tools>
+#
+# CC3_Blender_Tools is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# CC3_Blender_Tools is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with CC3_Blender_Tools.  If not, see <https://www.gnu.org/licenses/>.
+
 import bpy
 import os
 from . import materials, nodeutils, imageutils, properties, params, utils, vars
